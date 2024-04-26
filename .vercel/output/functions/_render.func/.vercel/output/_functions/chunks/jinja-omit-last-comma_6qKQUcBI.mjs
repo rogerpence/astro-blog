cf3a040e80c7c49cf80c06ea1798a7a993aa0add
Roger Pence <rp@rogerpence.com>
2024-04-26 14:55:06 -0500

    index

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/jinja-omit-last-comma_6qKQUcBI.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./jinja-omit-last-comma_CaYemWXc.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/jinja-white-space_C98X9IwO.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./jinja-white-space_8S9jUxzC.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
