22553c5abbe93481c5a0e65cdbea95d5931626fe
Roger Pence <rp@rogerpence.com>
2024-05-11 23:51:33 -0500

    move json file

--- src/pages/api/tags.json.js
+++ src/pages/api/tags.json.js
@@ -7,5 +7,21 @@ export async function GET({ params, request }) {
   const j = { name: "roger" };
 
   //return new Response(JSON.stringify(tags_list, null, 2));
-  return new Response(JSON.stringify(j, null, 2));
+  return new Response(JSON.stringify(j, null, 2), {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
+
+export const get = ({ params, request }) => {
+  return new Response(
+    JSON.stringify({ name: "Hrishikesh", url: "http://hrishikeshpathak.com" }),
+    {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    }
+  );
+};
