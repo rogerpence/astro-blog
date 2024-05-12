2a4fa273f0609abc8d07341021cd9e1e7d803bab
Roger Pence <rp@rogerpence.com>
2024-05-11 23:18:57 -0500

    move json file

--- src/pages/api/tags.json.js
+++ src/pages/api/tags.json.js
@@ -1,7 +1,7 @@
 // Outputs: /builtwith.json
 // export const prerender = false;
 
-import { tags_list } from "../../../cmd-line/data/tag-objects.js";
+import { tags_list } from "../../data/tag-objects.js";
 
 export async function GET({ params, request }) {
   return new Response(JSON.stringify(tags_list, null, 2));
